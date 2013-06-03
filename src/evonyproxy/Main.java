564190901a2b0e79e750abbb55acb45b6476dcb7
Pilsburgh <archibald@choomogo.com>
2013-06-03 05:36:40 -0500

    Added code to Main to allow for the input of an Evony Server IP through a command argument. for instance "java -jar Efony.jar 64.156.195.60"

--- src/evonyproxy/Main.java
+++ src/evonyproxy/Main.java
@@ -4,18 +4,23 @@
  */
 package evonyproxy;
 
+import evonyproxy.common.LoggerFactory;
 import evonyproxy.connectors.EvonyPolicy;
 import evonyproxy.connectors.EvonyConnectorFactory;
 import evonyproxy.connectors.EvonyServer;
 import evonyproxy.connectors.EvonyClient;
-import java.net.*;
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Initialises Efony. Appears to be hard coded.
+ *
  * @author HP_Administrator
  */
 public class Main {
+
+    static final Logger log = LoggerFactory.newLogger(Main.class.getName());
 
     /**
      * @param args the command line arguments
@@ -28,7 +33,7 @@ public class Main {
         /* Datas is a context containing the Flash Policy and Evony Login info */
         Datas dat = new Datas(io);
         io.setDat(dat);
-        
+
 //        EvonyServer server = new EvonyServer(io,true, 443, "64.156.195.60");
 //        server.connectToServer();
 //        while(!server.getSocket().isConnected()) {}
@@ -36,17 +41,28 @@ public class Main {
 //        Thread serverThread = new Thread(server);
 
         /* Starts a connection to an Evony Server */
-        Thread server = new Thread(new EvonyServer(io,verbose,443,"64.156.195.60"),"EvonyServer");
-        server.start();
+        Thread server = null;
+        if (args[0] != null) {
+            try {
+                server = new Thread(new EvonyServer(io, verbose, 443, args[0]), "EvonyServer");
+                server.start();
+            } catch (Exception ioe) {
+                log.log(Level.SEVERE, "{0}is not a valid IP for an Evony server.", args[0]);
+            }
+        } else {
+            /* If you want to hard code an Evony server, do it here */
+            server = new Thread(new EvonyServer(io, verbose, 443, "64.156.195.60"), "EvonyServer");
+            server.start();
+        }
 
         /* Starts a policy listener that listens for the policy that all 
          * flash programs transmit before they start up.
          */
-        Thread policyConnector = new Thread(new EvonyConnectorFactory(io,verbose, EvonyPolicy.class.getName()), "policyConnector");
+        Thread policyConnector = new Thread(new EvonyConnectorFactory(io, verbose, EvonyPolicy.class.getName()), "policyConnector");
         /* Listens for Evony Clients. Their configurations need to be modified
          * to point to a local port.
          */
-        Thread clientConnector = new Thread(new EvonyConnectorFactory(io,verbose, EvonyClient.class.getName()), "clientConnector");
+        Thread clientConnector = new Thread(new EvonyConnectorFactory(io, verbose, EvonyClient.class.getName()), "clientConnector");
 
 
 //        serverThread.start();
